60b208b208c5bc903369aab208954abf01f8a97a
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-02 11:14:18 +1000

    image upload functioning

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,13 +7,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- ruby = Category.create({name:"ruby"})
- ruby.save 
- 
- java = Category.create({name:"java"})
- java.save 
+ruby = Category.create(name: 'ruby')
+ruby.save
 
- tony = User.create({
+java = Category.create(name: 'java')
+java.save
+
+html = Category.create(name: 'HTML')
+html.save
+
+css = Category.create(name: 'CSS')
+css.save
+
+python = Category.create(name: 'python')
+python.save
+
+javascript = Category.create(name: 'JavaScript')
+javascript.save
+
+fortran = Category.create(name: 'Fortran')
+fortran.save
+
+tony = User.create(
   email: 'tony@hotsexyman.com',
   password: 'tony12345',
   password_confirmation: 'tony12345',
@@ -21,11 +38,11 @@
   location: 'Melbourne',
   admin: true,
   aboutme: 'I have a cute little dog Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repellendus assumenda unde odit debitis hic expedita iusto cumque ducimus possimus!'
- })
- tony.save
-puts "just made a tony"
+)
+tony.save
+puts 'just made a tony'
 
- brad = User.create({
+brad = User.create(
   email: 'brad@hotsexyman.com',
   password: 'brad12345',
   password_confirmation: 'brad12345',
@@ -35,6 +52,6 @@ puts "just made a tony"
   location: 'Sydney',
   admin: true,
   aboutme: 'I have a cute little dog Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repellendus assumenda unde odit debitis hic expedita iusto cumque ducimus possimus!'
- })
- brad.save
-puts "just made a brad"
+)
+brad.save
+puts 'just made a brad'
