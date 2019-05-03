90e44ecb72354463ce2034f29501458fc0169a0a
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-03 11:12:46 +1000

    .

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,10 +58,9 @@ class PostsController < ApplicationController
   end
 
   def send_email
-    # raise
-    @user = User.find(@post.user_id)
-
-    UserMailer.with(user: @user).interested_in_post_email.deliver_now
+    @postuser = User.find(@post.user_id)
+    @user = current_user
+    UserMailer.with(user: @postuser).interested_in_post_email.deliver_now
     redirect_to root_path
     # redirect to a prompt that tells the user they have been successful sending an email and the poster will be in contact with them soon
   end
