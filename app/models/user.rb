01c5b8c7e3ea8bc95944d8236ef2523c22f94888
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-06 11:11:21 +1000

    added trackable field to devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
   has_one_attached :avatar
   has_many :posts
 end
