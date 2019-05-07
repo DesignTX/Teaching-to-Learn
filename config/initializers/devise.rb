700801e4798b3791790a3f9534b01cea58907d63
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-07 12:07:01 +1000

    added facebook-login functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,13 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], scope: 'public_profile,email',
+                                                                         client_options: {
+                                                                           site: 'https://graph.facebook.com/v2.11',
+                                                                           authorize_url: 'https://www.facebook.com/v2.11/dialog/oauth'
+                                                                         }
+
+  config.omniauth :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@
 
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get '/sendemail/:id', to: 'posts#send_email', as: 'email'
   resources :posts
   resources :transactions
   resources :comments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
