b8ac7134fcd18451fc3d84d891acb8db0f4fe1aa
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-08 14:39:19 +1000

    set up the testing environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,21 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/poltergeist'
+require 'factory_girl_rails' #With require methods we load files from the new added gems, so we could use their methods below.
+require 'capybara/rspec'
+
+config.include Devise::Test::IntegrationHelpers, type: :feature #This configuration allows us to use devise methods inside capybara tests. It was provided inside the Devise docs.
+config.include FactoryGirl::Syntax::Methods #This configuration allows to use factory_girl gem’s methods.
+Capybara.javascript_driver = :poltergeist
+Capybara.server = :puma 
+
+
+
+
+
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,7 +53,25 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
-
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+ 
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+ 
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+ 
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+ 
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
