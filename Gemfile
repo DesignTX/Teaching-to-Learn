15adebee539cc03f4ffb6478962a64ec6be1a1e1
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-08 12:12:20 +1000

    ..

--- Gemfile
+++ Gemfile
@@ -81,6 +81,7 @@ group :test do
   gem 'webdrivers', '~> 3.0'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rails-controller-testing'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,13 +1,30 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
-
-  test "should not save post without required fields" do
-    article = Article.new
-    assert_not article.save
+  def setup
+    @user = users(:admin)
   end
+
+  test "non-signed in user layout links" do
+    get root_path
+    assert_template 'posts/index'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", new_user_session_path, count: 2
+    assert_select "a[href=?]", new_user_registration_path
+  end
+
+  # test "signed in user layout links" do
+  #   get new_user_session_path
+  #   assert_template 'devise/sessions/new'
+  #   post user_session_path, params: { user: { email: @user.email, password: 'password' } }
+  #   assert_not flash.empty?
+  #   assert_redirected_to root_path
+  #   follow_redirect!
+  #   assert_template 'static_pages/home'
+  #   assert_select "a[href=?]", new_user_session_path, count: 0
+  #   assert_select "a[href=?]", new_user_registration_path, count: 0
+  #   assert_select "a[href=?]", edit_user_registration_path
+  #   assert_select "a[href=?]", destroy_user_session_path
+  # end
 end
