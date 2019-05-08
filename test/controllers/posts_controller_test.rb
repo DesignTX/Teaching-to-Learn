75f713375da2a5693faf0be0c9beccca2c934681
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-08 11:16:20 +1000

    added testing-gems

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,10 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "should not save post without required fields" do
+    article = Article.new
+    assert_not article.save
+  end
 end
