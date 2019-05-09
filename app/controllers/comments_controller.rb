45a954f043eb2aa318c513c54ecabc76e8b2b7cf
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-09 12:06:52 +1000

    styled show page added media queries

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   end
 
   def new
-    @comment = Comment.new(post_id: params[:post_id])
+    @comment = Comment.new(post_id: params[:user_id])
     @post = Post.find(params[:post_id])
   end
 
